8bd5657086dab8007c9a4538ec265a81d36ba004
ltk2118 <65684902+ltk2118@users.noreply.github.com>
2021-11-08 21:46:51 -0500

    Add cipher

---
+++ cipher.py
@@ -0,0 +1,20 @@
+def cipher(text, shift, encrypt=True):
+    """Cipher function that we want to test"""
+    assert isinstance(shift, (int, float)) #part e
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
+
+
+
+
+##
+
